31ff79b2751befc8a54d352edf2ba097667e7fc2
Your Name <you@example.com>
2023-10-14 08:12:51 -0700

    printf project

---
+++ 1-main.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+int main(void)
+{
+	const char *data_to_process = "meeeee";
+	_print_buffer(data_to_process);
+
+	return (0);
+}
--- main.h
+++ main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 int _printf(const char *format, ...);
 void _putchar(char c);
 int print_number(int n);
@@ -9,5 +13,5 @@ int print_unsigned(unsigned int n);
 int print_octal(unsigned int n);
 int print_hex(unsigned int n, int uppercase);
 int print_address(void *p);
-
+void _print_buffer(const char *data);
 #endif
